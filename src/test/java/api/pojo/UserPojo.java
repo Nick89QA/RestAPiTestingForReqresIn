dc8b3da236126d756ec31c00ad8782f3108e310c
Nickolay Petrych <npetrych@alfabank.ru>
2023-09-22 16:57:41 +0100

    add pojo

---
+++ src/test/java/api/pojo/UserPojo.java
@@ -0,0 +1,34 @@
+package api.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Data
+
+public class UserPojo {
+        private Integer id;
+        private String email;
+        @JsonProperty("first_name")
+        private String first_name;
+        @JsonProperty("last_name")
+        private String last_name;
+        private String avatar;
+
+//        private Integer page;
+//        private Integer per_page;
+//        private Integer total;
+//        private Integer total_pages;
+//        private ArrayList<UserPojo> data;
+//        private Support support;
+//
+//    public class Support {
+//        private String url;
+//        private String text;
+//    }
+}
+
+
