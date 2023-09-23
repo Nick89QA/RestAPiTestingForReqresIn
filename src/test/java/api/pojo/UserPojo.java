429f557ce4886fff8e9631ba20f896d922f551f9
Nickolay Petrych <npetrych@alfabank.ru>
2023-09-23 11:47:19 +0100

    add a couple methods

--- src/test/java/api/pojo/UserPojo.java
+++ src/test/java/api/pojo/UserPojo.java
@@ -10,25 +10,25 @@ import java.util.ArrayList;
 @Data
 
 public class UserPojo {
-        private Integer id;
-        private String email;
-        @JsonProperty("first_name")
-        private String first_name;
-        @JsonProperty("last_name")
-        private String last_name;
-        private String avatar;
+    private Integer id;
+    private String email;
+    @JsonProperty("first_name")
+    private String first_name;
+    @JsonProperty("last_name")
+    private String last_name;
+    private String avatar;
 
-//        private Integer page;
-//        private Integer per_page;
-//        private Integer total;
-//        private Integer total_pages;
-//        private ArrayList<UserPojo> data;
-//        private Support support;
-//
-//    public class Support {
-//        private String url;
-//        private String text;
-//    }
+    private Integer page;
+    private Integer per_page;
+    private Integer total;
+    private Integer total_pages;
+    private ArrayList<UserPojo> data;
+    private Support support;
+
+    public class Support {
+        private String url;
+        private String text;
+    }
 }
 
 
