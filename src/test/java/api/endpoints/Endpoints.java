e00517b373e79b433db33f8b75452502d2f4b37c
Nickolay Petrych <npetrych@alfabank.ru>
2023-10-02 12:21:50 +0100

    add negative scenarios for method get

--- src/test/java/api/endpoints/Endpoints.java
+++ src/test/java/api/endpoints/Endpoints.java
@@ -5,8 +5,13 @@ public class Endpoints {
     public static final String getSingleUser = "/api/users/2";
     public static final String getSingleUserNotFound = "/api/users/23";
     public static final String getListResource = "/api/unknown";
-    public static final String getSingleResource = "api/unknown/2";
+    public static final String getSingleResource = "/api/unknown/2";
     public static final String URL = "https://reqres.in";
+    public static final String incorrectUrl = "https://reqr.in";
+    public static final String incorrectPathGetSingleUser = "/apiuses";
+
+    public static final String postUser = "/api/users";
+    public static final String putUser = "/api/users/2";
 
 
 
--- src/test/java/api/tests/UsersList.java
+++ src/test/java/api/tests/UsersList.java
@@ -16,16 +16,22 @@ import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.equalTo;
 
 public class UsersList {
 
     private static final String getListUsers = Endpoints.getListUsers;
+    private static final String getSingleUser = Endpoints.getSingleUser;
     private static final String URL = Endpoints.URL;
+    private static final String incorrectUrl = Endpoints.incorrectUrl;
+    private static final String incorrectPathGetSingleUser = Endpoints.incorrectPathGetSingleUser;
 
     private static final Logger logger = MyLogger.getLogger();
 
-
+    /**
+     * this test checks and asserts from response email and name
+     */
     @Test
     public void getListOfUsersAndMatchEmailAndFirstName() {
         given()
@@ -36,10 +42,13 @@ public class UsersList {
                 .then().log().all().statusCode(200)
                 .body("data[1].email", equalTo("janet.weaver@reqres.in"))
                 .body("data[0].first_name", equalTo("George"));
-        logger.info("--We match email and name from JSonFile--: PASSED");
+        logger.info("--We match email from 'id2' and name from 'id1' in the JSonFile--: PASSED");
 
     }
 
+    /**
+     * this test asserts all the emails and avatars from response
+     */
     @Test
     public void getListOfUsers() {
         List<UserPojo> users = given()
@@ -48,7 +57,7 @@ public class UsersList {
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then()
-                .statusCode(200).log().all().statusCode(200)
+                .log().all().statusCode(200)
                 .extract().jsonPath().getList("data", UserPojo.class);
         users.forEach(x -> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
 
@@ -58,4 +67,84 @@ public class UsersList {
         Assertions.assertTrue(users.stream().allMatch(x -> x.getAvatar().endsWith("jpg")));
         logger.info("--We match all avatars and format is jpg--: PASSED");
     }
+
+    /**
+     * this test checks and asserts response from single user
+     */
+    @Test
+    public void getSingleUser() {
+        given()
+                .baseUri(URL)
+                .basePath(getSingleUser)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .log().body().statusCode(200)
+                .body("data.email", equalTo("janet.weaver@reqres.in"))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.last_name", equalTo("Weaver"));
+        logger.info("--We match email, first name,last name from single user: PASSED");
+
+    }
+
+    /**
+     * this test checks statusCode with incorrect url
+     */
+    @Test
+    public void checksStatusCodeWIthIncorrectUrl() {
+        given()
+                .baseUri(incorrectUrl)
+                .basePath(getSingleUser)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .log().all().statusCode(404);
+    }
+
+    /**
+     * this test checks statusCode with incorrect base path
+     */
+    @Test
+    public void checksStatusCodeWIthIncorrectPath() {
+        given()
+                .baseUri(URL)
+                .basePath(incorrectPathGetSingleUser)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .log().all().statusCode(404);
+    }
+
+    /**
+     * this test assert incorrect email in response
+     */
+    @Test
+    public void assertIncorrectEmail() {
+        given()
+                .baseUri(URL)
+                .basePath(getSingleUser)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .log().all()
+                .body("data.email",not(equalTo("janet.weaverreqres.in")));
+
+    }
+    /**
+     * this test assert incorrect first name in response
+     */
+    @Test
+    public void assertIncorrectFirstName() {
+        given()
+                .baseUri(URL)
+                .basePath(getSingleUser)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .log().all()
+                .body("data.first_name",not(equalTo("Jant")));
+
+    }
+
+
 }
